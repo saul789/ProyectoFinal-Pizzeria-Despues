9dfcc0314032d344e7e3f185f019c4441c8d135d
saul789 <75595443+saul789@users.noreply.github.com>
2022-05-12 21:34:44 -0600

    Update BD.sql

--- BD.sql
+++ BD.sql
@@ -5,3 +5,10 @@ CREATE TABLE Persons (
     Address varchar(255),
     City varchar(255)
 );
+CREATE TABLE Persons (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
